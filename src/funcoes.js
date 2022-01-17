bcf6e5cf77223853af49d4062db51a66eea85b92
Victor de Medeiros Pacheco <victordemedeiros5@gmail.com>
2022-01-17 02:55:07 -0300

    commit para iniciar a ramificação

---
+++ src/funcoes.js
@@ -0,0 +1,97 @@
+// =========
+// Essencial
+// =========
+
+// Escreva uma função que receba um nome e retorne uma saudação para este nome: Tiago -> Olá, Tiago
+
+function saudar() { 
+    let nome = "Tiago";
+    console.log("Olá, " nome );
+}
+// Escreva uma função que receba um nome completo e retorna apenas o primeiro nome: Tiago Lage Payne de Pádua -> Tiago
+function extrairPrimeiroNome() {
+ let nomeCompleto = "Tiago Lage Payne de Pádua" ;
+ let nome01 = nomeCompleto.split("");
+ alert(nome01[0]);
+}
+
+// Escreva uma função que receba uma palavra e torna a primeira letra maiúscula e as outras minúsculas: tIaGo -> Tiago
+function capitalizar() {
+ let palavra = "tIago";
+ console.log(palavra[0].toUpperCase() + palavra.slice(1).toLowerCase());
+    }
+
+// Escreva uma função que recebe um preço original e uma categoria de produto e calcula o valor do imposto. Produtos da categoria Alimentação são isentos. Outros produtos tem um imposto de 10%.
+// (30, Alimentação) => 0
+// (10, Bebida) => 1
+function calculaImposto() {
+    let valorOriginal = 30;
+    let categoria = "Alimentação";
+    let valorFinal = 0;
+
+    if (categoria === "Alimentação" ); {
+        valorOriginal = valorFinal
+        console.log(valorFinal)
+    }
+    else {
+        valorOriginal*1.10 = valorFinal
+        console.log(valorFinal)
+    } 
+ }
+
+// Escreva uma função que recebe um preço original, uma categoria de produto e um cupom de desconto e calcula o preço com desconto. Se a categoria for Alimentação e o cupom for NULABSSA, deve ser feito um desconto de 50%. Caso contrário, não há nenhum desconto.
+// (30, Alimentação, NULABSSA) => 15
+// (10, Bebida, NULABSSA) => 10
+// (30, Alimentação, XPTO) => 30
+// (10, Bebida, XPTO) => 10
+function calculaDesconto() { 
+    let valorOriginal = 30;
+    let valorFinal = 15;
+    let categoria = "Alimentação";
+    let cupom = "NULABSSA";
+    if (categoria === "Alimentação" && cupom === "NULABSSA" ){
+    valorOriginal*0.50 = valorFinal 
+    console.log(valorFinal)
+ }
+    else  {
+        console.log(valorFinal)
+    }
+}
+
+// =========
+// Desejável
+// =========
+
+// Escreva uma função que receba uma palavra como primeiro argumento, um comprimento máximo como segundo argumento e trunca a palavra se ela for maior que o comprimento máximo.
+// o valor default do comprimento máximo deve ser 5:
+// (teste, 10) -> teste
+// (fulano, 4) -> fula...
+function truncar() { 
+    let palavra = "teste";
+    let tamanho = 10 ;
+    let palavraFinal="";
+    if(palavra.length>= tamanho + 3){
+        palavraFinal = palavra.substring(0,tamanho).concat("...");
+    }
+    console.log (palavraFinal)
+}
+
+// Escreva uma função que valida se o texto informado está preenchido e retorna o texto sem espaços antes ou depois.
+// "" -> undefined
+// "   " -> undefined
+// "      Maria " -> "Maria"
+function validaTextoPreenchido() { 
+    let texto =""
+    console.log(texto.trim())
+}
+
+// =======
+// Desafio
+// =======
+
+// Escreva uma função que valida se a string passada é uma data de nascimento válida, deve retornar um objeto Date sea data for válida ou NaN caso seja inválida.
+// 01/01/2000 -> Ok
+// 99/99/9999 -> NaN
+function validaData() { }
+
+module.exports = { saudar, extrairPrimeiroNome, capitalizar, calculaImposto, calculaDesconto, truncar, validaTextoPreenchido, validaData };
